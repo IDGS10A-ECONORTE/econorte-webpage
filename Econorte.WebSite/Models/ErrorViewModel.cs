71f87f674b223a3ee1d63f126827bd15f4f5a580
Elias <martinezfloreseliasrafael123@gmail.com>
2025-10-05 00:00:51 -0600

    Subir la solución y los proyectos para el WebSite de econorte

---
+++ Econorte.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Econorte.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
