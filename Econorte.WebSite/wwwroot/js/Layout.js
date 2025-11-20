f5f3017ffea6ea863f4452a3c3cc47c952db0725
Elias <martinezfloreseliasrafael123@gmail.com>
2025-11-19 18:15:05 -0600

    bugfix de bucle infinito.

--- Econorte.WebSite/wwwroot/js/Layout.js
+++ Econorte.WebSite/wwwroot/js/Layout.js
@@ -2,7 +2,7 @@
 
 // Rutas generadas desde el layout
 const loginUrl = login;
-const indexUrl = index;
+const indexUrl = "/";
 
 // Rutas públicas (se pueden visitar sin autenticarse)
 const publicPages = [
@@ -14,14 +14,14 @@ const publicPages = [
 const token = Auth.getToken();
 
 // Función para verificar si la URL actual coincide con una pública
-const isPublicPage = publicPages.some(p => currentUrl.startsWith(p));
+const isPublicPage = publicPages.some(p => currentUrl.endsWith(p));
 
-// 1. Si NO hay token y NO es página pública → redirige a login
+// 1. Si NO hay token y NO es página pública redirige a login
 if (!token && !isPublicPage) {
     window.location.href = loginUrl;
 }
 
-// 2. Si HAY token y estás en login → redirige al index
+// 2. Si HAY token y estás en login redirige al index
 else if (token && currentUrl.startsWith(loginUrl)) {
     window.location.href = indexUrl;
 }
